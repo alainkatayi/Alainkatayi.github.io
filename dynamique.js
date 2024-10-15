b514bd2d7644e5915e5e4d72b3d3e3ce2aecf57a
alainMH <alainkatayi01@gmail.com>
2024-10-16 04:51:34 +0530

    finition, quelques modifications, et mise en place des animations et du premier media query

---
+++ dynamique.js
@@ -0,0 +1,53 @@
+var cont = document.getElementById("Contact");
+cont.addEventListener('click', MenuContact);
+
+function MenuContact(){
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("popup").style.display = "Block"
+}
+
+
+var fermer = document.getElementById("fermerButton");
+fermer.addEventListener("click",fermerButton );
+
+function fermerButton(){
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("popup").style.display = "none"
+}
+
+
+
+var cont2 = document.getElementById("Contact2");
+cont2.addEventListener('click', MenuContact);
+
+function MenuContact(){
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("popup").style.display = "Block"
+}
+
+
+var fermer = document.getElementById("fermerButton");
+fermer.addEventListener("click",fermerButton );
+
+function fermerButton(){
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("popup").style.display = "none"
+}
+
+
+var cont3 = document.getElementById("Contact3");
+cont3.addEventListener('click', MenuContact);
+
+function MenuContact(){
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("popup").style.display = "Block"
+}
+
+
+var fermer = document.getElementById("fermerButton");
+fermer.addEventListener("click",fermerButton );
+
+function fermerButton(){
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("popup").style.display = "none"
+}
